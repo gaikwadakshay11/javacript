0beb8c868d6445b8c0236dab07075c02fef6806a
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-07 12:39:57 +0000

    conversion of data type

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,22 @@
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ let isLoggedIn ="akshay"
+ let boolenIsLoggedIn= Boolean(isLoggedIn)
+  
+ // console.log(booleanIsLoggedIn);
+// "" => false
+// "akshay"=> true
+
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+
+  console.log(typeof stringNumber);

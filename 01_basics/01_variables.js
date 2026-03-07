09fbc0c1a92802b16b2e6c734683e75fbd251af6
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-07 13:38:26 +0000

    variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+ let accountEmail = "akshay@gmail.com"
+ var accountPassword ="123456"
+ accountCity ="Latur"
+ let accounState;
+
+ accountEmail ="akshay@gmail.com"
+ accountPassword= "1233234"
+//  accountCity ="Latur"
+
+ console.log(accountId);
+ console.log([accountId,accountEmail,accountPassword,accountCity,accounState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";
+// alert(3+3) // we are using nodejs, not brower
+console.log(3 + 3) // code readablitiy to high
+; console.log("Akshay")
+

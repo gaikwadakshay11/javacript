986fd86400b50f7440c6e55e418c23022c8b3774
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-07 13:35:15 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// primative data type 7 = String, Boolearn, null, Number, undefined, symbol, BigInt
+
+const score =100
+const scoreValue= 100.3
+const osLoggedIn = false
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+// const bigNumber = 342342343442343
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros=["shaktiman", "naagraj", "doga"]
+let myObj = {
+     name:"akshay",
+     age:22,
+
+}
+const myfunction = function() {
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);

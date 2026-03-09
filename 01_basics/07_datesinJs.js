df8d61473511b59f78e4f197b4cc6c81e93e35e9
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-09 10:37:48 +0000

    datasinJs pratice

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,10 @@
+// Current Date
+// let date = new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+
+let myDate = new Date()
+// let myCreatedDate = new Date (2026, 2, 9)
+let myCreatedDate = new Date (2026, 1, 23, 5, 3)
+console.log(myCreatedDate.toDateString());

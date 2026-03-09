fd7bfda4f0f48e916d7aaecf64c49df2174536b4
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-09 05:01:33 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "akshay"
+const repoCount = 40
+
+console.log(name +repoCount+ "value");
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('akshay')
+console.log(gameName[0]);
+console.log(gameName._proto_);
+ 
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+

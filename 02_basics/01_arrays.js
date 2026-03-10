caea02ce2b6450afb2ee96fe208c302631149c9a
Akshay <137155394+gaikwadakshay11@users.noreply.github.com>
2026-03-10 05:29:33 +0000

    Arrays of flat concat

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -19,12 +19,37 @@ const myArr2 = new Array(1,2,3,4)
 // console.log(myArr);
 // console.log(typeof newArr);
 
-console.log("A",myArr);
-const myn1 = myArr.slice(1,3)
+// console.log("A",myArr);
+// const myn1 = myArr.slice(1,3)
 
-console.log("B", myArr);
-console.log(myn1);
+// console.log("B", myArr);
+// console.log(myn1);
 
-const myn2 = myArr.splice(1,3)
-console.log("c",myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1,3)
+// console.log("c",myArr);
+// console.log(myn2);
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superaman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros[3][1]);
+
+const allheros = marvel_heros.concat(dc_heros)
+console.log(allheros);
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+ 
+
+const another_array =[1,2,3,[4,5,6],7,[4,5,[6,7]]]
+const real_another_array= another_array.flat(Infinity)
+console.log(real_another_array);
+console.log(Array.isArray("Akshay"))
+console.log(Array.from("akshay"))
+console.log(Array.from({name:"akshay"}))  // interesting
+
+let scroe1 = 100
+let scroe2 = 200
+let scroe3 = 300
+ 
+console.log(Array.of (scroe1,scroe2,scroe3));
